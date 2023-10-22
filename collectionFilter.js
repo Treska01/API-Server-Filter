f92bf6e23130d9cc175f8607443412ed7847c2b5
Treska01 <treska01@hotmail.com>
2023-10-22 18:28:22 -0400

    first commit

---
+++ collectionFilter.js
@@ -0,0 +1,12 @@
+export default class CollectionFilter {
+    constructor (objects, params, model) {
+        this.objects = objects;
+        this.params = params;
+        this.model = model;
+    }
+    get() {
+        let objectsList = this.objects;
+        let 
+        return objectsList;
+    }
+}
